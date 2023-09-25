25efd37ff2e6a10486e00d588863de2cb2b5c8d4
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-09-25 22:03:46 +0200

    Fix bad whitespace input format

--- cramptes2.py
+++ cramptes2.py
@@ -1,9 +1,9 @@
 import re
 
 def verifyInput(input):
-  if (re.match(r'^(f\(x\)=)?([a-zA-Z]|\d)(\*|\+|\-|\/|\**)*\d+$', input)):
+  if (re.match(r'^(f\(x\)\s?=\s?)?([a-zA-Z]|\d)\s?(\*|\+|\-|\/|\**)*\s?\d+', input)):
     return True
   return False
 
-print(verifyInput('f(x)=x*323'))
+print(verifyInput('f(x) = x * 323'))
 print(verifyInput('x**2'))
